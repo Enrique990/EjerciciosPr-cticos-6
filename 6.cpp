7eeb62843b7e43515327cbe7a33fc5d1af26f056
Enrique990 <voidenrique@gmail.com>
2024-05-02 13:58:48 -0600

    sexto ejercicio completado

---
+++ 6.cpp
@@ -0,0 +1,22 @@
+/* Hacer un programa que sume los números pares comprendidos entre
+100 y 200.*/
+
+//sin copilot
+
+#include<iostream>
+using namespace std;
+
+int suma = 0;
+
+int main(int argc, char const *argv[])
+{
+    for (int i = 100; i <= 200; i++)
+    {
+        if (i % 2 ==0)
+        {
+            suma += i;
+        }
+    }
+    cout << "la suma de los numeros pares entre 100 y 200 es: "<<suma;
+    return 0;
+}
