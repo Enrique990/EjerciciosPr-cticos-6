3c1a696f9817bbd8f8f9074a18daaf651bb481d3
Enrique990 <voidenrique@gmail.com>
2024-05-02 19:50:39 -0600

    correccion del ejercicio 7

--- 7.cpp
+++ 7.cpp
@@ -7,41 +7,31 @@ resultado debe mostrarse en un tercer vector*/
 #include <iostream>
 using namespace std;
 
-int main()
+int main(int argc, char const *argv[])
 {
-    int longitud = 0;
-    int array1[longitud];
-    int array2[longitud];
-    int array3[longitud];
-
-    cout << "Ingresa la longitud del vector: ";
+    int longitud;
+    cout << "Ingrese la longitud de los vectores: ";
     cin >> longitud;
+    int vector1[longitud];
+    int vector2[longitud];
+    int producto = 0;
 
-    cout << endl
-         << "Ingresando datos para el vector 1..." << endl
-         << endl;
     for (int i = 0; i < longitud; i++)
     {
-        cout << "Posicion [ " << i + 1 << " ] = Dime un numero: ";
-        cin >> array1[i];
+        cout << "ingrese el valor del vector 1 en la posicion "<<i<<": ";
+        cin >> vector1[i];
     }
-
-    cout << endl
-         << "Ingresando datos para el vector 2..." << endl
-         << endl;
     for (int i = 0; i < longitud; i++)
     {
-        cout << "Posicion [ " << i + 1 << " ] = Dime un numero: ";
-        cin >> array2[i];
+        cout << "ingrese el valor del vector 2 en la posicion "<<i<<": ";
+        cin >> vector2[i];
     }
-
-    cout << endl
-         << "Calculando resultados..." << endl;
     for (int i = 0; i < longitud; i++)
     {
-        array3[i] = +(array1[i] + array2[i]);
-        cout << "Posicion [ " << i + 1 << " ] = " << array3[i] << endl;
+        producto += vector1[i]+vector2[i];
     }
+    cout << "el producto de los dos vectores es: "<<producto;
+    return 0;
 }
 
 //con copilot
